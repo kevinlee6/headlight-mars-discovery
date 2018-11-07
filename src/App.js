c45f6428ff5c9c947fba032f41ce64cd01e906df
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2018-11-06 20:28:34 -0500

    Change title of app and created Container component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Container from './components/Container';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container-fluid">
+        <Container />
       </div>
     );
   }
