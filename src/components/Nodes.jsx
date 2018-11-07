32ee286561c984f0f4bbdb4390afe42dc07f3c15
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2018-11-06 21:06:17 -0500

    Implement Node helper tag creator.

--- src/components/Nodes.jsx
+++ src/components/Nodes.jsx
@@ -5,7 +5,7 @@ export default class Nodes extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      nodes: [],
     };
   }
 
--- src/helper.jsx
+++ src/helper.jsx
@@ -2,3 +2,20 @@ import React from 'react';
 
 export const url = (endpoint = '') =>
   `https://headlight-tournament-3.herokuapp.com/${endpoint}`;
+
+export const toPercent = x => (x * 100) + '%';
+
+export const Node =
+  (key, nodeTop, nodeLeft, value, gridSize = 20) => {
+    const top = toPercent(nodeTop/gridSize);
+    const left = toPercent(nodeLeft/gridSize);
+    const width = (100 / gridSize) + '%';
+    const height = width;
+
+    return <div
+      key={key}
+      style={{top, left, width, height}}
+      className='node'>
+      {value}
+    </div>
+};
