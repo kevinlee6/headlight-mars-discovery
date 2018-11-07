bf33c9b57fc6aaca7e87ca7349587bc44e14cf87
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2018-11-06 20:40:15 -0500

    Add Scores component.

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 export const Info = props => (
   <div>
-
+    <h1>Mars Challenge</h1>
+    <p>Sample React.js app that pulls node and bot info from Headlights API.</p>
+    <p>URL used: {url}</p> 
+    <p>Endpoints (GET): nodes or bots</p>
+    <Scoreboard bots={props.bots} />
   </div>
 );
---
+++ src/components/Scores.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Scores = props => {
+  <table className='table table-hover'>
+
+  </table>
+}
